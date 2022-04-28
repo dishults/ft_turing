98e304ed2645851104561d1b6c650be6aa65b89a
Dmytro Shults <dishults@icloud.com>
2022-04-28 15:30:40 +0200

    Add parsing and update json file structure

--- ft_turing.py
+++ ft_turing.py
@@ -4,7 +4,10 @@ import argparse
 
 
 def main():
-    pass
+    parser = argparse.ArgumentParser()
+    parser.add_argument('jsonfile', help='json description of the machine')
+    parser.add_argument('input', help='input of the machine')
+    args = parser.parse_args()
 
 
 if __name__ == "__main__":
